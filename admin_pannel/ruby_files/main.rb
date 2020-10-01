a8ac8e6cca76aa4d9856cf5cf18b5160ee44b501
mohitkhurana91 <72201496+mohitkhurana91@users.noreply.github.com>
2020-10-01 17:08:35 +0530

    Update main.rb

--- admin_pannel/ruby_files/main.rb
+++ admin_pannel/ruby_files/main.rb
@@ -3,8 +3,8 @@ require_relative 'crud'
 users = [
           { username: "gitesh", password: "password1" },
           { username: "jack", password: "password2" },
-          { username: "mohit", password: "password3" },
-          { username: "jonshow", password: "password4" },
+          { username: "mohit91", password: "password3" },
+          { username: "rjrohit", password: "password4" },
           { username: "heisenberg", password: "password5" }
         ]
 
